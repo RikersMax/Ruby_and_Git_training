3f8e89d3e0ca9dd7c5e1834b65a95c20d8be3ab9
RikersMax <maksimlarionov112@icloud.com>
2023-01-08 11:28:08 +0500

    Coding

--- dice.rb
+++ dice.rb
@@ -1,8 +1,12 @@
 # encoding: cp1251
 x = 0
+summ = 0
 6.times do
   x = rand(0..6)
+  summ += x
   print("\r#{x}")
   sleep 1
 end
-puts ("\nYou got #{x}")
+puts("\nYou got #{x}")
+puts("all sum - #{summ}")
+
