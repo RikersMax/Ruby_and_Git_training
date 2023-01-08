d3a1e982a93d1fed7ba693932ac2eff6599da6c1
RikersMax <maksimlarionov112@icloud.com>
2023-01-08 10:16:32 +0500

    Version 3

--- dice.rb
+++ dice.rb
@@ -1,6 +1,15 @@
 # encoding: cp1251
-puts('How many dice')
-num = gets.to_i
-num.times do
-  puts (rand(6))
+#puts('How many dice')
+#num = gets.to_i
+5.times do
+x = (rand(0..6))
+if x == 0
+  puts ('edge')
+else
+  if x.even?
+    puts ('eagle')
+  elsif x.odd?
+    puts ('tails')
+  end
+end
 end
