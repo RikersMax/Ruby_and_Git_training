ee6f116803296454e8bce746d34e8de0a22a97ff
RikersMax <maksimlarionov112@icloud.com>
2023-02-01 21:02:35 +0500

    Admin mode

--- barb.rb
+++ barb.rb
@@ -1,24 +1,52 @@
 require 'sinatra'
 require 'colorize'
 
+logfile = Dir.entries('./clients').drop(2)
+
 get '/' do
-	erb :welcome2
+	erb :welcome1
+end
+
+get '/admin' do
+  erb :welcome2
+end
+
+get '/clear' do
+  File.open('./clients/test.txt', 'w').write('')
+  erb :message2
+end
+
+post '/admin' do
+	@Admin_name = params[:Admin_name]
+	@Admin_pass = params[:Admin_pass]
+
+	if @Admin_name == 'admin' && @Admin_pass == 'admin'
+		@title = 'Welcome You admin'
+		@mess = 'choice file'
+		@logfile = File.read('./clients/test.txt')
+
+		erb :message2
+	else
+		@title = 'access'
+		@mess = 'denied'
+		erb :message2
+	end
 end
 
 post '/' do
-   	@name = params[:User_name]
+  @name = params[:User_name]
 	@number = params[:Phone]
 	@date = params[:Date_time]
 
-	@title = 'Thank you'
-	@mess = "Dear <i style=\"color: red\">#{@name}</i> well be waiting for you at #{@date} <sup>2023</sup>"
-	@mess2 = "We are you calling by phone <em style=\"color:blue\">#{@number}</em> <sub>your number</sub> <mark>This is mark</mark>"
-	
+  if @name
 	clients_path = File.dirname(__FILE__) + '/clients/'
-	f = File.new(clients_path + "#{@name}.txt", 'w')
-	f.puts('Name - ' + @name.capitalize)
-	f.puts('Phone - ' + @number)
-	f.puts('Date - ' + @date)
-	erb :message2
+	f = File.open(clients_path + "test.txt", 'a')
+	f.puts('Name - ' + "<i style=\"color :green\">#{@name.capitalize}</i>"+ '<br \>')
+	f.puts('Phone - ' + @number + '<br \>')
+	f.puts('Date - ' + @date + '<br \>')
+  f.puts('=================<br \>')
+	erb :message1
+	end
 
 end
+
